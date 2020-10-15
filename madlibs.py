ba62e5277240e93ad2cd8e8ca39a63291585a8eb
vagrant <vagrant@vagrant.vm>
2020-10-15 20:06:23 +0000

    revised compliment and game html files

--- madlibs.py
+++ madlibs.py
@@ -44,19 +44,24 @@ def greet_person():
 
 @app.route('/game')
 def show_madlib_form():
-    game_input = input("Would you like to play a game? N or Y?")
-    if game_input == 'N':
-        return render_template("goodbye.html")
-    if game_input == 'Y':
+
+    play_game = request.args.get("play")
+
+    if play_game == "yes":
         return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
 
 @app.route('/madlib')
 def show_madlib():
+    name = request.args.get("person")
     color = request.args.get('color')
     noun = request.args.get('noun')
     adjective = request.args.get('adjective')
 
     return render_template("madlib.html",
+                            person=name,
                             color=color,
                             noun=noun,
                             adjective=adjective)        
